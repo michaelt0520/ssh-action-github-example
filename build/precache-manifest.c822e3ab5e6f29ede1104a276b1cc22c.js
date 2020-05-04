95eae4ff147c669b2c035e392ebeae60081fd8be
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2020-05-04 21:42:12 +0700

    yarn run build

---
+++ build/precache-manifest.c822e3ab5e6f29ede1104a276b1cc22c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c78d188ba585135084bc5580a7e2750d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5545d3f5ffbb02950207",
+    "url": "/static/css/2.aa419733.chunk.css"
+  },
+  {
+    "revision": "a0c713ffb0bb9f7d2f78",
+    "url": "/static/css/main.136fc84a.chunk.css"
+  },
+  {
+    "revision": "5545d3f5ffbb02950207",
+    "url": "/static/js/2.d18ad8d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d18ad8d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0c713ffb0bb9f7d2f78",
+    "url": "/static/js/main.5bf41c98.chunk.js"
+  },
+  {
+    "revision": "2b1966e609115edb2dc0",
+    "url": "/static/js/runtime-main.787cdf2b.js"
+  }
+]);
